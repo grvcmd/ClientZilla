fccd1b325e8623301274ca754955edf3b3f5d600
EstebanCam <70626890+EstebanCam@users.noreply.github.com>
2022-02-28 13:58:51 -0600

    Created api to get all of the clients from the external api

--- routes/client.js
+++ routes/client.js
@@ -4,3 +4,16 @@ const router = express.Router()
 
 //don't need an import since it's an external api?
 
+// api that will fetch data from the external api 
+//using the await function 
+router.get('/async', async (req, res, next) => {
+    let apiURL= 'https://cis-4339.herokuapp.com/api/v1/data'
+    try{
+        const response = await axios.get(apiURL);
+        res.status(200).json(response.data);
+
+    } catch(err){
+        res.status(500).json({message: err});
+    }
+
+});
