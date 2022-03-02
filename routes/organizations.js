328e6ba7017a2b6aa9e4d131f41bd04cbc913d97
Ken Valverde <kenvalverde@Kens-MacBook-Air.local>
2022-03-01 20:12:10 -0600

    Start on routes regarding organizations.

---
+++ routes/organizations.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router;
+
+// Import Organization model
+const OrgModel = require('./../models/organization')
+
+// Endpoint to get all organizations
+
+
+// Endpoint to GET org by orgID
+
+
+// Endpoint to DELETE org by orgID
+
+
+// Endpoint to POST an org
+
+
+// Endpoint to PUT an existing org
