62d300dac683a104f2eef8c5c0b0955d216b33ba
Ken Valverde <kenvalverde@Kens-MacBook-Air.local>
2022-04-09 16:54:34 -0500

    Start on SignUpView.vue

---
+++ cis-4339-project-frontend/src/views/SignUpView.vue
@@ -0,0 +1,25 @@
+<template>
+
+</template>
+
+<script>
+    import axios from "axios";
+
+    export default {
+        data() {
+            return {
+                intakeForm: {
+                    firstName: '',
+                    lastName: '',
+                    phoneNumber: '',
+                    age: '',
+                    gender: '',
+                    isVaccinated: '', // boolean
+                    isVeteran: '',  // boolean
+                    numOfDependants: '',    // int
+                }
+            }
+        }
+    }
+
+</script>
