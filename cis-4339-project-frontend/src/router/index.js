69af2ca344d338426ac4b0f86a91cf5b2ad12ba9
Ken Valverde <kenvalverde@Kens-MacBook-Air.local>
2022-04-09 18:42:12 -0500

    Fix misspelling and comment routers with uncreated views.

--- cis-4339-project-frontend/src/router/index.js
+++ cis-4339-project-frontend/src/router/index.js
@@ -1,11 +1,11 @@
 //importing router
-import { createRouter, createdWebHistory} from 'vue-router'
+import { createRouter, createWebHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 //below we're creating the routes
 //importing components & views from their local loaction 
 const router = createRouter({
-    history: createdWebHistory(import.meta.env.BASE_URL),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/',
@@ -16,29 +16,25 @@ const router = createRouter({
             // intake form view for new clients to fill
             path: '/signup',
             name: 'signup',
-            component: () => import('../components/SignUpView.vue')
+            component: () => import('../views/SignUpView.vue')
 
         },
-        {
-            path: '/view',
-            name: 'view',
-            component: () => import('../components/ListComponent.vue')
-        },
-        {
-            path:'/edit/:id',
-            name: 'edit',
-            component: () => import('../components/EditComponent.vue')
-        },
-        {
-            path: '/about',
-            name: 'about',
-            component: () => import('../views/AboutView.vue')
-        }
+        // {
+        //     path: '/view',
+        //     name: 'view',
+        //     component: () => import('../components/ListComponent.vue')
+        // },
+        // {
+        //     path:'/edit/:id',
+        //     name: 'edit',
+        //     component: () => import('../components/EditComponent.vue')
+        // },
+        // {
+        //     path: '/about',
+        //     name: 'about',
+        //     component: () => import('../views/AboutView.vue')
+        // }
     ]
-
-
-
-
 })
 
 export default router
