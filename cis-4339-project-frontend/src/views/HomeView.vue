ee4f3f56bd982b795dcb654bf7722d1618d1788c
Ken Valverde <kenvalverde@Kens-MacBook-Air.local>
2022-04-09 18:43:22 -0500

    Create HomeView

---
+++ cis-4339-project-frontend/src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<script>
+// import child component
+import HelloWorld from '../components/HelloWorld.vue'
+export default {
+  // register child component
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      msg: "You really did it!"
+    }
+  }
+}
+</script>
+
+<template>
+  <main>
+    <div class="wrapper">
+      
+      <!-- <HelloWorld msg="You did it yeah!" />  -->
+      <HelloWorld :msg="msg" />
+    </div>
+  </main>
+</template>
+
+<style>
+/* default styling */
+@import "../assets/base.css";
+</style>
