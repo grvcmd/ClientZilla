413cdfa87f43b9ef826d0e31d04d1f262ecd2df8
EstebanCam <70626890+EstebanCam@users.noreply.github.com>
2022-04-11 21:16:20 -0500

    Finished creating org view to display a table of all orgs

--- cis-4339-project-frontend/src/views/OrganizationView.vue
+++ cis-4339-project-frontend/src/views/OrganizationView.vue
@@ -10,12 +10,10 @@
         </tr>
       </thead>
       <tbody>
-       <tr v-for="student in Students" :key="student._id">
-          <td>{{ student.firstName }}</td>
-          <td>{{ student.lastName }}</td>
-          <td>{{ student.studentID }}</td>
-          <td>{{ student.email }}</td>
-          <td>{{ student.phoneNumber }}</td>
+       <tr v-for="organization in Organizations" :key="organization._id">
+          <td>{{ organization.orgID }}</td>
+          <td>{{ organization.orgName }}</td>
+          <td>{{ organization.orgDesc }}</td>
             <!-- <td>
             <router-link :to="{name: 'edit', params: { id: student._id }}" class="btn btn-success ">Edit</router-link>
             <button @click.prevent="deleteStudent(student._id)" class="btn btn-danger mx-2">Delete</button>
@@ -26,4 +24,25 @@
   </div>
 </template>
 
-//Going to 
+//Going to import axios to call backend api 
+ 
+<script>
+    import axios from "axios";
+export default {
+    data (){
+        return {
+            Organizations:[]
+        }
+    },
+    created(){
+        let apiURL = 'http://localhost:3001/organizations';
+        axios.get(apiURL).then(res =>{
+            this.Organizations = res.data;
+        }).catch(error =>{
+            console.log(error)
+        });    
+        
+    }
+    
+}
+</script>
