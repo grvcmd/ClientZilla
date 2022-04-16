7a972671eb620f38888ca65fd23fe2cfd0608571
Ken Valverde <kenvalverde@Kens-MacBook-Air.local>
2022-04-16 03:21:47 -0500

    Change homeview

--- cis-4339-project-frontend/src/views/HomeView.vue
+++ cis-4339-project-frontend/src/views/HomeView.vue
@@ -15,16 +15,17 @@ export default {
 </script>
 
 <template>
-  <main>
-    <div class="wrapper">
-      
-      <!-- <HelloWorld msg="You did it yeah!" />  -->
-      <HelloWorld :msg="msg" />
+  <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+    <div class="col-md-5 p-lg-5 mx-auto my-5">
+      <h1 class="display-5 fw-bold">ClientZilla</h1>
+      <p class="lead font-weight-normal">Effectively handle your clients and services - all in one application.</p>
     </div>
-  </main>
+    <!-- <div class="product-device box-shadow d-none d-md-block"></div>
+    <div class="product-device product-device-2 box-shadow d-none d-md-block"></div> -->
+  </div>
 </template>
 
 <style>
 /* default styling */
-@import "../assets/base.css";
+/* @import "../assets/base.css"; */
 </style>
