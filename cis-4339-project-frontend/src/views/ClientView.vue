98b8e0686971e0d1e80c3afc4ce0034136e9a677
Ken Valverde <kenvalverde@Kens-MacBook-Air.local>
2022-04-18 10:27:05 -0500

    Only display basic information about the client.

--- cis-4339-project-frontend/src/views/ClientView.vue
+++ cis-4339-project-frontend/src/views/ClientView.vue
@@ -7,33 +7,34 @@
           <th>Orgs Worked With</th>
           <th>First Name</th>
           <th>Last Name</th>
-          <th>Phone Number</th>
-          <th>Age</th>
-          <th>Gender</th>
-          <th>SSN</th>
-          <th>Vaccinated</th>
-          <th>Veteran</th>
-          <th>Number of Dependants</th>
+          <!-- <th>Phone Number</th> -->
+          <!-- <th>Age</th> -->
+          <!-- <th>Gender</th> -->
+          <!-- <th>SSN</th> -->
+          <!-- <th>Vaccinated</th> -->
+          <!-- <th>Veteran</th> -->
+          <!-- <th>Number of Dependants</th> -->
           <th>Client ID</th>
-          <th>Created At</th>
-          <th>Updated At</th>
+          <!-- <th>Created At</th> -->
+          <!-- <th>Updated At</th> -->
         </tr>
       </thead>
       <tbody>
        <tr v-for="intakeForm in intakeForms" :key="intakeForm._id">
-          <td>{{ intakeForm.orgsWorkedWith}}</td>
+          <td>{{intakeForm.orgsWorkedWith.join(', ')}}</td>
+          <!-- <td>{{ intakeForm.orgsWorkedWith}}</td> -->
           <td>{{ intakeForm.firstName}}</td>
           <td>{{ intakeForm.lastName}}</td>
-          <td>{{ intakeForm.phoneNumber}}</td>
+          <!-- <td>{{ intakeForm.phoneNumber}}</td>
           <td>{{ intakeForm.age}}</td>
           <td>{{ intakeForm.gender}}</td>
           <td>{{ intakeForm.ssn}}</td>
           <td>{{ intakeForm.isVaccinated}}</td>
           <td>{{ intakeForm.isVeteran}}</td>
-          <td>{{ intakeForm.numOfDependants}}</td>
+          <td>{{ intakeForm.numOfDependants}}</td> -->
           <td>{{ intakeForm.clientID}}</td>
-          <td>{{ intakeForm.createdAt}}</td>
-          <td>{{ intakeForm.updatedAt}}</td>
+          <!-- <td>{{ intakeForm.createdAt}}</td>
+          <td>{{ intakeForm.updatedAt}}</td> -->
           <!-- <td>
             <router-link :to="{name: 'program-services', params: { id: program.programID }}" class="btn btn-success ">View Services</router-link>
           </td> -->
