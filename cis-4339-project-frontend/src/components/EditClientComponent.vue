e9563bee14e46e35630940e04eedf2f827d01672
Ken Valverde <kenvalverde@Kens-MacBook-Air.local>
2022-04-18 17:22:25 -0500

    Change heading

--- cis-4339-project-frontend/src/components/EditClientComponent.vue
+++ cis-4339-project-frontend/src/components/EditClientComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="row justify-content-center">
         <div class="col-md-6">
-            <h3 class="text-center">Client Registration</h3>
+            <h3 class="text-center">Update Client</h3>
             <form @submit.prevent="handleUpdateForm">
                 <div class="form-group">
                     <label>Organizations you have worked with</label>
